1132b24d6fa497efdfd0be273d0e0c8458aa0a1e
almmtv <76550816+almmtv@users.noreply.github.com>
2021-05-03 14:01:05 +0300

    Update finhub_data.py

--- finhub_data.py
+++ finhub_data.py
@@ -1,35 +1,60 @@
 import requests
-r = requests.get('https://finnhub.io/api/v1/quote?symbol=AMZN&token=c240262ad3iaqj3tjgng')
-print('\n_____________Financial quote___________\n')
-print('Current price:', r.json()['c'])
-print('Open price of the day:', r.json()['o'])
-print('High price of the day:', r.json()['h'])
-print('Low price of the day:', r.json()['l'])
-print('Previous close price:', r.json()['pc'])
-print('\n______________COMPANY NEWS________________\n')
-r = requests.get('https://finnhub.io/api/v1/company-news?symbol=AMZN&from=2021-04-28&to=2021-04-28&token=c240262ad3iaqj3tjgng')
-for news in r.json():
-    print('headline: ', news['headline'])
-    print('summary: ', news['summary'])
-    print('url: ', news['url'])
-print('\n______________News Sentiment______________\n')
-r = requests.get('https://finnhub.io/api/v1/news-sentiment?symbol=AMZN&token=c240262ad3iaqj3tjgng')
-print('Sector Average Bullish Percent: ', r.json()['sectorAverageBullishPercent'])
-print('Sector Average News Score: ', r.json()['sectorAverageNewsScore'])
-print('Bearish percent: ', r.json()['sentiment']['bearishPercent'])
-print('Bullish Percent: ', r.json()['sentiment']['bullishPercent'])
-print('\n______________Basic Financials____________\n')
-r = requests.get('https://finnhub.io/api/v1/stock/metric?symbol=AMZN&metric=all&token=c240262ad3iaqj3tjgng')
-for metric in r.json()['metric']:
-    print(metric, r.json()['metric'][metric])
-print('\n______________Insiders Transaction________________\n')
-r = requests.get('https://finnhub.io/api/v1/stock/insider-transactions?symbol=AMZN&from=2021-02-12&to=2021-04-27&token=c240262ad3iaqj3tjgng')
-for operation in r.json()['data']:
-    for detail in operation:
-        print(detail, ': ', operation[detail])
-    print('')
-print('_________________Recommendation Trends____________\n')
-r = requests.get('https://finnhub.io/api/v1/stock/recommendation?symbol=AMZN&token=c240262ad3iaqj3tjgng')
-for recommendation in r.json()[0]:
-    if recommendation != 'symbol':
-        print(recommendation, r.json()[0][recommendation])
+from multiprocessing.pool import ThreadPool
+import sys
+
+
+def financial_quote_link(stock_name, api_key):
+    link = 'https://finnhub.io/api/v1/quote?symbol=' + stock_name + '&token=' + api_key
+    return link
+
+
+# date format: YYYY-MM-DD
+def company_news_link(stock_name, date_from, date_to, api_key):
+    link = 'https://finnhub.io/api/v1/company-news?symbol=' + stock_name + '&from=' + date_from + '&to=' + date_to + \
+           '&token=' + api_key
+    return link
+
+
+def news_sentiment_link(stock_name, api_key):
+    link = 'https://finnhub.io/api/v1/news-sentiment?symbol=' + stock_name + '&token=' + api_key
+    return link
+
+
+def basic_financials_link(stock_name, api_key, metric='all'):
+    link = 'https://finnhub.io/api/v1/stock/metric?symbol=' + stock_name + '&metric=' + metric + '&token=' + api_key
+    return link
+
+
+# date format: YYYY-MM-DD
+def insiders_transaction_link(stock_name, date_from, date_to, api_key):
+    link = 'https://finnhub.io/api/v1/stock/insider-transactions?symbol=' + stock_name + '&from=' + date_from + \
+           '&to=' + date_to + '&token=' + api_key
+    return link
+
+
+def recommendation_trends_link(stock_name, api_key):
+    link = 'https://finnhub.io/api/v1/stock/recommendation?symbol=' + stock_name + '&token=' + api_key
+    return link
+
+
+def request(link):
+    try:
+        r = requests.get(link)
+    except requests.exceptions.ConnectionError:
+        print('Connection error')
+        return None
+    if str(r) == '<Response [401]>':
+        print('Wrong token')
+        return None
+    return r.json()
+
+
+stock = sys.argv[-1]
+token = 'c240262ad3iaqj3tjgng'
+links = [financial_quote_link(stock, token), company_news_link(stock, '2021-05-02', '2021-05-03', token),
+         news_sentiment_link(stock, token), basic_financials_link(stock, token),
+         insiders_transaction_link(stock, '2021-02-03', '2021-05-03', token), recommendation_trends_link(stock, token)]
+pool = ThreadPool(8)
+results = pool.map(request, links)
+pool.close()
+pool.join()
